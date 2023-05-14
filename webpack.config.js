5bba8158490dca6b6cb37863e206dd4d25be4354
Shimon Doodkin <helpmepro1@gmail.com>
2023-05-14 20:12:10 +0300

    add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,18 @@ module.exports = {
     filename: 'dist-api-accessor.js',
     library: "ApiAccessor",
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
